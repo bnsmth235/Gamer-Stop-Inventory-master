f5e2e1710ea2213c4c3a05ebe3b7289deaecf685
Benjamin Smith <bnsmth235@gmail.com>
2018-04-13 08:02:28 -0600

    test
    what tghdcv

--- Gamer Stop Inventory/src/Inventory.java
+++ Gamer Stop Inventory/src/Inventory.java
@@ -348,7 +348,8 @@ public class Inventory {
 				if(find_line.toLowerCase().contains(console.toLowerCase())){
 					maxIndx++;
 					list[maxIndx]=find_line;
-				}
+				}//hello
+		
 			}
 			System.out.println();
 		}
