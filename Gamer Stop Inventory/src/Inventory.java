55ae454ac3883552fbc2a90213c4207490b48788
Benjamin Smith <bnsmth235@gmail.com>
2018-04-24 18:42:19 -0600

    final?
    A bunch of stuff and added buttons, mostly just bug fixes and moved everything more towards the center of the screen

--- Gamer Stop Inventory/src/Inventory.java
+++ Gamer Stop Inventory/src/Inventory.java
@@ -17,11 +17,13 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.event.*;
 import java.util.*;
 import java.io.*;
 
-public class inventory {
+public class Inventory {
 	
 	public static Scanner in=new Scanner(System.in);
 	public static String srch_in;
@@ -37,9 +39,12 @@ public class inventory {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		frame=new JFrame("Gamer Stop Inventory"); //UI start 
-		frame.setSize(1000, 500);
+		frame.setSize(950, 300);
+		frame.setLocation(400, 300);
 		frame.setLayout(null);	
 		frame.setVisible(true);
+		Container b=frame.getContentPane();
+		b.setBackground(Color.orange);
 		
 		JLabel title= new JLabel("Are you a customer or an Employee?");
 		title.setBounds(333,25,666,75);
@@ -71,13 +76,15 @@ public class inventory {
 	}
 	
 	public static void employee(){
-		
 		frame.setVisible(false);//gets rid of old window
-				
+		
 		JFrame login_frame = new JFrame("Employee Login");
 		login_frame.setSize(300, 300);	
+		login_frame.setLocation(700,300);
 		login_frame.setLayout(null); //new window setup
 		login_frame.setVisible(true);
+		Container c=login_frame.getContentPane();
+		c.setBackground(Color.orange);
 				
 		final JPasswordField pass = new JPasswordField(); //Password field text entry
 		pass.setBounds(100,75,100,30);   
@@ -98,8 +105,8 @@ public class inventory {
 		r.setBounds(100, 160, 90, 30);
 		
 		JButton back=new JButton("Back to Main");
-		back.setBounds(20, 250, 115, 30);
-			    
+		back.setBounds(86, 200, 115, 30);		    
+		
 		login_frame.add(pass); login_frame.add(l1); login_frame.add(l2); login_frame.add(b); 
 		login_frame.add(user); login_frame.add(r); login_frame.add(back);//add all components to frame
 			    
@@ -134,9 +141,12 @@ public class inventory {
 				login_frame.setVisible(false);
 			    		
 			    JFrame register=new JFrame("Register New Employee");
-			    register.setSize(350,350);
+			    register.setSize(300,350);
 			    register.setLayout(null);
 			    register.setVisible(true);
+			    register.setLocation(700,300);
+			    Container c=register.getContentPane();
+				c.setBackground(Color.orange);
 			    		
 			    JLabel reg_label=new JLabel("Welcome new employee!");
 			    reg_label.setBounds(90,15,200,25);
@@ -163,7 +173,7 @@ public class inventory {
 				b.setBounds(130,220, 90,30);   //login button 
 				
 				JButton back= new JButton("Back to Login");
-				back.setBounds(10, 280, 115, 30);
+				back.setBounds(116, 260, 115, 30);
 					    
 				register.add(user); register.add(pass); register.add(reg_label); register.add(l2);
 			    register.add(b); register.add(l1); register.add(l3); register.add(confirm); register.add(back);
@@ -205,9 +215,14 @@ public class inventory {
 	@SuppressWarnings("rawtypes")
 	public static void e_search_frame() throws IOException{
 		JFrame e_frame=new JFrame("Employee Inventory Search");
-		e_frame.setSize(500, 500);
+		e_frame.setSize(350, 400);
+		e_frame.setLocation(400, 350);
 		e_frame.setLayout(null);
 		e_frame.setVisible(true);
+		Container c=e_frame.getContentPane();
+		c.setBackground(Color.orange);
+		
+		JFrame add_frame=new JFrame("Add Game Info");
 		
 		JLabel e_start=new JLabel("Search for a Title by Name or Console");//title at top of frame
 		e_start.setBounds(50,15,666,75);
@@ -224,15 +239,92 @@ public class inventory {
 		e_title.setBounds(100, 150, 180, 30);
 		
 		JButton e_search=new JButton("Search");
-		e_search.setBounds(100, 220, 80, 30);//enter button basically
+		e_search.setBounds(90, 220, 80, 30);//enter button basically
+		
+		JButton back=new JButton("Back to Main");
+		
+		JButton add=new JButton("Add");
+		add.setBounds(170, 220, 70, 30);
+		add.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent add) {
+				add_frame.setSize(1100, 160);
+				add_frame.setLocation(400, 750);
+				add_frame.setLayout(null);
+				add_frame.setVisible(true);
+				Container c=add_frame.getContentPane();
+				c.setBackground(Color.orange);
+				
+				JTextField title=new JTextField();
+				title.setBounds(60, 10, 150, 30);
+				JLabel title_label=new JLabel("Title:");
+				title_label.setBounds(20,10,50,30);
+				
+				JTextField console=new JTextField();
+				console.setBounds(270, 10, 150, 30);
+				JLabel console_label=new JLabel("Console:");
+				console_label.setBounds(215,10,50,30);
+				
+				JTextField price=new JTextField();
+				price.setBounds(490, 10,150, 30);
+				JLabel price_label=new JLabel("Price:");
+				price_label.setBounds(448,10,50,30);
+				
+				JTextField condition=new JTextField();
+				condition.setBounds(710, 10, 150, 30);
+				JLabel condition_label=new JLabel("Condition:");
+				condition_label.setBounds(650,10,70,30);
+				
+				JTextField cost=new JTextField();
+				cost.setBounds(930, 10, 150, 30);
+				JLabel cost_label=new JLabel("Cost:");
+				cost_label.setBounds(890,10,50,30);
+				
+				JButton enter=new JButton("Enter");
+				enter.setBounds(420, 70, 80, 30);
+				enter.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent a) {
+						String title1=title.getText();
+						String console1=console.getText();
+						String price1=price.getText();
+						String condition1=condition.getText();
+						String cost1=cost.getText();
+						
+						try {
+							FileWriter(title1,console1,price1,condition1,cost1);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						add_frame.setVisible(false);
+					}
+				});
+				
+				JButton cancel=new JButton("Cancel");
+				cancel.setBounds(520, 70, 80, 30);
+				cancel.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent b) {
+						add_frame.setVisible(false);
+					}
+				});
+				
+				add_frame.add(title); add_frame.add(title_label);add_frame.add(console_label); add_frame.add(console); add_frame.add(price);
+				add_frame.add(condition); add_frame.add(cost);add_frame.add(cost_label);add_frame.add(condition_label);add_frame.add(price_label);
+				add_frame.add(enter); add_frame.add(cancel);
+				
+				
+				
+				
+				}
+		});
 		
 		e_frame.add(e_start); e_frame.add(e_consoles); e_frame.add(e_title_label); 
-		e_frame.add(e_title); e_frame.add(e_search); //add all components to the frame
+		e_frame.add(e_title); e_frame.add(e_search); e_frame.add(back); e_frame.add(add); //add all components to the frame
 		
 		JFrame e_inventory_list=new JFrame("Searched Inventory");
-		e_inventory_list.setSize(600, 400);
+		e_inventory_list.setSize(750, 400);
 		e_inventory_list.setLayout(null);
-		e_inventory_list.setLocation(600, 0);
+		e_inventory_list.setLocation(750, 350);
+		Container a=e_inventory_list.getContentPane();
+		a.setBackground(Color.orange);
 		
 		DefaultTableModel model=new DefaultTableModel();
 		model.addColumn("Title"); model.addColumn("Console"); model.addColumn("Price for Sale");
@@ -255,7 +347,7 @@ public class inventory {
 			    try {
 			    	String list[]=new String[1000];
 			    	int maxIndx=-1;
-			    	Scanner scan=new Scanner(new File(school));
+			    	Scanner scan=new Scanner(new File(home));
 
 			    	while(scan.hasNextLine()){
 			    		String find_line=scan.nextLine();
@@ -291,18 +383,34 @@ public class inventory {
 		});
 		JTable area=new JTable(model);
 		JScrollPane scroll=new JScrollPane(area);
-		scroll.setBounds(0,0,600,400);
+		scroll.setBounds(0,0,750,400);
 		scroll.setViewportView(area);
-		
 		e_inventory_list.setVisible(true);
 		e_inventory_list.add(scroll);
+		
+		back.setBounds(75, 300, 150, 30);
+		back.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent b) {
+				try {
+					e_frame.setVisible(false);
+					add_frame.setVisible(false);
+					e_inventory_list.setVisible(false);
+					main(null);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 	
 	public static void customer() throws IOException{
 		JFrame c_frame=new JFrame("Customer Inventory Search");
 		c_frame.setSize(1000, 500);
+		c_frame.setLocation(400, 200);
 		c_frame.setLayout(null);
 		c_frame.setVisible(true);
+		Container c=c_frame.getContentPane();
+		c.setBackground(Color.orange);
 		
 		JLabel c_start=new JLabel("Search for a Title by Name or Console");//title at top of frame
 		c_start.setBounds(50,15,666,75);
@@ -319,15 +427,29 @@ public class inventory {
 		title.setBounds(100, 150, 180, 30);
 		
 		JButton search=new JButton("Search");
-		search.setBounds(100, 220, 80, 30);//enter button basically
+		search.setBounds(75, 220, 80, 30);//enter button basically
 		
 		JTextArea area=new JTextArea();
-		area.setBounds(300,30,600,400);
 		JScrollPane scroll=new JScrollPane(area);
+		scroll.setBounds(300,30,600,400);
+		scroll.setViewportView(area);
 		area.setEditable(false);
 		
-		c_frame.add(area); c_frame.add(scroll); c_frame.add(c_start); c_frame.add(consoles); c_frame.add(title_label); 
-		c_frame.add(title); c_frame.add(search); //add all components to the frame
+		JButton back=new JButton("Back to Main");
+		back.setBounds(155, 220, 120, 30);
+		back.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent b){
+				c_frame.setVisible(false);
+				try {
+					main(null);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		c_frame.add(scroll); c_frame.add(c_start); c_frame.add(consoles); c_frame.add(title_label); 
+		c_frame.add(title); c_frame.add(search); c_frame.add(back); //add all components to the frame
 		
 		search.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent a){
@@ -350,14 +472,33 @@ public class inventory {
 	
 	
 	
-	public static void FileWriter(String word_and_def) throws IOException{
+	public static void FileWriter(String title, String console, String price, String condition, String cost) throws IOException{
 		
-		FileWriter fw=new FileWriter(school, true); //writes new registered employees
+		FileWriter fw=new FileWriter(home, true); //writes new registered employees
 		PrintWriter pw=new PrintWriter(fw, true);
-		pw.println(word_and_def); //File Writing Method so I don't have to type this every time
-		pw.close();
-		fw.close();
-		
+		if(price.contains("$")) {
+			if(cost.contains("$")) {
+				String format_cost=cost.substring(1, cost.length());
+				String format_price=price.substring(1, price.length());
+				pw.println(title+", "+console+", "+"$"+format_price+", "+condition+", "+"$"+format_cost); //File Writing Method so I don't have to type this every time
+				pw.close();
+				fw.close();
+			}else if(price.contains("$")) {
+				String format_price=price.substring(1, price.length());
+				pw.println(title+", "+console+", "+"$"+format_price+", "+condition+", "+"$"+cost); //File Writing Method so I don't have to type this every time
+				pw.close();
+				fw.close();
+			}else if(cost.contains("$")) {
+				String format_cost=cost.substring(1, cost.length());
+				pw.println(title+", "+console+", "+"$"+price+", "+condition+", "+"$"+format_cost); //File Writing Method so I don't have to type this every time
+				pw.close();
+				fw.close();
+			}
+		}else{
+			pw.println(title+", "+console+", "+"$"+price+", "+condition+", "+"$"+cost); //File Writing Method so I don't have to type this every time
+			pw.close();
+			fw.close();
+		}
 	}
 	/**
 	 * 
@@ -367,7 +508,7 @@ public class inventory {
 	 * @throws IOException
 	 */
 	public static boolean FileReader_user_pass(String user, String pass) throws IOException{
-		Scanner scan=new Scanner(new File(school_user_pass)); //reads username and password lists
+		Scanner scan=new Scanner(new File(home_user_pass)); //reads username and password lists
 		while(scan.hasNextLine()){
 			final String find_line=scan.nextLine();
 			if(find_line.toLowerCase().contains(user.toLowerCase())){//checks if the word (lower case) is in any words or 
@@ -384,7 +525,7 @@ public class inventory {
 	public static String FileReader_c_game_titles(String title, String console) throws IOException{ //reads list of game titles
 		String list[]=new String[1000];
 		int maxIndx=-1;
-		Scanner scan=new Scanner(new File(school));
+		Scanner scan=new Scanner(new File(home));
 		System.out.println(console);
 		while(scan.hasNextLine()){
 			final String find_line=scan.nextLine();
